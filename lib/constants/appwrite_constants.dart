f72166f9c889d49d85475e1cf570c7175a4918d2
Jiju <20jiju@gmail.com>
2023-07-05 15:42:54 +0530

    feat: Add assets and inital folder setup

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = "64a5250349449f7fddd0";
+  static const String projectId = "64a3b1a32ae1f731d1fd";
+  static const String endPoint = "http://localhost:80";
+}
