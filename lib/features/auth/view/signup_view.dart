d5b63a3c4d1c62131894194668ad4f976484b641
Jiju <20jiju@gmail.com>
2023-07-06 08:09:11 +0530

    feat: Add login/signup view and appwrite setup

--- lib/features/auth/view/signup_view.dart
+++ lib/features/auth/view/signup_view.dart
@@ -1,25 +1,24 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:twitter_clone/common/loading_page.dart';
-import 'package:twitter_clone/common/rounded_small_button.dart';
+import 'package:twitter_clone/common/common.dart';
 import 'package:twitter_clone/constants/constants.dart';
 import 'package:twitter_clone/features/auth/controller/auth_controller.dart';
-import 'package:twitter_clone/features/auth/view/signup_view.dart';
+import 'package:twitter_clone/features/auth/view/login_view.dart';
 import 'package:twitter_clone/features/auth/widgets/auth_field.dart';
-import 'package:twitter_clone/theme/pallete.dart';
+import 'package:twitter_clone/theme/theme.dart';
 
-class LoginView extends ConsumerStatefulWidget {
+class SignUpView extends ConsumerStatefulWidget {
   static route() => MaterialPageRoute(
-        builder: (context) => const LoginView(),
+        builder: (context) => const SignUpView(),
       );
-  const LoginView({super.key});
+  const SignUpView({super.key});
 
   @override
-  ConsumerState<LoginView> createState() => _LoginViewState();
+  ConsumerState<SignUpView> createState() => _SignUpViewState();
 }
 
-class _LoginViewState extends ConsumerState<LoginView> {
+class _SignUpViewState extends ConsumerState<SignUpView> {
   final appbar = UIConstants.appBar();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
@@ -31,8 +30,8 @@ class _LoginViewState extends ConsumerState<LoginView> {
     passwordController.dispose();
   }
 
-  void onLogin() {
-    ref.read(authControllerProvider.notifier).login(
+  void onSignUp() {
+    ref.read(authControllerProvider.notifier).signUp(
           email: emailController.text,
           password: passwordController.text,
           context: context,
@@ -67,20 +66,20 @@ class _LoginViewState extends ConsumerState<LoginView> {
                       Align(
                         alignment: Alignment.topRight,
                         child: RoundedSmallButton(
-                          onTap: onLogin,
+                          onTap: onSignUp,
                           label: 'Done',
                         ),
                       ),
                       const SizedBox(height: 40),
                       RichText(
                         text: TextSpan(
-                          text: "Don't have an account?",
+                          text: "Already have an account?",
                           style: const TextStyle(
                             fontSize: 16,
                           ),
                           children: [
                             TextSpan(
-                              text: ' Sign up',
+                              text: ' Login',
                               style: const TextStyle(
                                 color: Pallete.blueColor,
                                 fontSize: 16,
@@ -89,7 +88,7 @@ class _LoginViewState extends ConsumerState<LoginView> {
                                 ..onTap = () {
                                   Navigator.push(
                                     context,
-                                    SignUpView.route(),
+                                    LoginView.route(),
                                   );
                                 },
                             ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:twitter_clone/features/auth/view/login_view.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import './theme/theme.dart';
+import 'common/common.dart';
+import 'features/auth/controller/auth_controller.dart';
+import 'features/auth/view/signup_view.dart';
+import 'features/home/view/home_view.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: AppTheme.theme,
-      home: const LoginView()
+      home: ref.watch(currentUserAccountProvider).when(
+            data: (user) {
+              if (user != null) {
+                return const HomeView();
+              }
+              return const SignUpView();
+            },
+            error: (error, st) => ErrorPage(
+              error: error.toString(),
+            ),
+            loading: () => const LoadingPage(),
+          ),
     );
   }
 }
