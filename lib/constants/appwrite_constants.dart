ca4ae335830e6479f4cf49109d0c509abf58ea9a
Jiju <20jiju@gmail.com>
2023-07-31 15:28:36 +0530

    feat: Add signup, login auth

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -1,5 +1,10 @@
 class AppwriteConstants {
   static const String databaseId = "64a5250349449f7fddd0";
   static const String projectId = "64a3b1a32ae1f731d1fd";
-  static const String endPoint = "http://localhost:80";
+  static const String endPoint = "http://localhost:80/v1";
+  // for running in android emulator
+  // adb root
+  // adb reverse tcp:80 tcp:80
+
+  static const String usersCollection = "64c75e75783c8838e29c";
 }
